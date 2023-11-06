877af3ebc14eeb3b2b3e7fc648bf3c8a193da4ca
Deiiivy <deivypr28@gmail.com>
2023-11-05 20:14:48 -0500

    mi repo

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const link1 = document.getElementById('link1');
+const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+
+
+link1.addEventListener('click', scrollToSection);
+link2.addEventListener('click', scrollToSection);
+link3.addEventListener('click', scrollToSection);
+
+
+function scrollToSection(event) {
+  event.preventDefault(); 
+
+ 
+  let targetSectionId = event.currentTarget.id.replace('link', ''); 
+
+
+  let targetSection = document.getElementById(`section${targetSectionId}`);
+
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
